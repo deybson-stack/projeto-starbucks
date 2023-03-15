0b3c0585b6589c2e1a2aa5a5a2c8a4ac09eff44c
deybson-stack <118633158+deybson-stack@users.noreply.github.com>
2023-03-14 23:41:22 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function slider(anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function changeColor(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color;
+}
